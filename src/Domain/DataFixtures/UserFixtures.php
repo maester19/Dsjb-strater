4766f02dce23241620952ce118ae67c5f9c5fbdc
maesterlyb <yves.ekanga27@gmail.com>
2021-05-11 17:15:02 +0100

    feat: Connection & securite

---
+++ src/Domain/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Domain\DataFixtures;
+
+use App\Domain\Auth\Users;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+
+     private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new Users();
+        $user->setNom("maester");
+        $user->setPassword($this->encoder->encodePassword($user,"maesterpro"));
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
